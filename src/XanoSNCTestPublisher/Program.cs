4531e7ff484c13f5fbf82240795746454e5c89a9
Greg Cummines <greg_cummines@starkey.com>
2015-10-29 13:06:38 -0500

    Added create notification event test method.

--- src/XanoSNCTestPublisher/Program.cs
+++ src/XanoSNCTestPublisher/Program.cs
@@ -15,16 +15,40 @@ namespace XanoServiceNotificationCenterTestPublisher
     /// </summary>
     class Program
     {
+        static string baseAddress = "http://localhost:8733/XanoServiceNotificationCenter";
+
         static void Main()
         {
-            SendNotification();
+            Publisher_CreateNotificationEvent();
+            Publisher_SendNotification();
         }
 
-        static async void SendNotification()
+        static void Publisher_CreateNotificationEvent()
         {
             using (var httpClient = new HttpClient())
             {
-                httpClient.BaseAddress = new Uri("http://localhost:8733/XanoServiceNotificationCenter");
+                httpClient.BaseAddress = new Uri(baseAddress);
+                string json = "{ \"FirmwarePackageVersion\": \"5.0.1.9\", \"FirmwareConfigurationVersion\": \"9.1.1.3.0\" }";
+                var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var url = "notifySubscribers/Roka/FirmwareRelease";
+                var response = httpClient.PostAsync(url, stringContent).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Success");
+                }
+                else
+                {
+                    Console.WriteLine("Error. The status code was " + response.StatusCode);
+                }
+            }
+        }
+
+        static async void Publisher_SendNotification()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(baseAddress);
                 string json = "{ \"FirmwarePackageVersion\": \"5.0.1.9\", \"FirmwareConfigurationVersion\": \"9.1.1.3.0\" }";
                 var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
 
