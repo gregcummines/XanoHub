c9b97ae0f1d6793fd354b4e3bd9d6f25b1d4c278
Greg Cummines <gregcummines@gmail.com>
2015-10-30 16:40:46 -0500

    Added projects for subscriber testing.

---
+++ src/XanoSNCTestSubscriberWindowsService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XanoSNCTestSubscriberWindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if !DEBUG
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new XanoSNCTestSubscriberWindowsService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#else
+            var service = new XanoSNCTestSubscriberWindowsService();
+            service.DebugThisService();
+#endif
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ src/XanoSNCTestSubscriberWindowsService/XanoSNCTestSubscriberWindowsService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using XanoSNCTestSubscriberLibrary;
+
+namespace XanoSNCTestSubscriberWindowsService
+{
+    public partial class XanoSNCTestSubscriberWindowsService : ServiceBase
+    {
+        private ServiceHost wcfXanoSNCTestSubscriber;
+
+        public XanoSNCTestSubscriberWindowsService()
+        {
+            InitializeComponent();
+        }
+
+        public void DebugThisService()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            wcfXanoSNCTestSubscriber = new ServiceHost(typeof(XanoSNCTestSubscriber));
+            wcfXanoSNCTestSubscriber.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (wcfXanoSNCTestSubscriber != null && wcfXanoSNCTestSubscriber.State == CommunicationState.Opened)
+            {
+                wcfXanoSNCTestSubscriber.Close();
+            }
+        }
+    }
+}
